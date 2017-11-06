7b5d179eaf9e868f504d6c0576068ff820556290
Cédric Despierre Corporon <cedric.despierre@gmail.com>
2017-11-06 20:13:41 +0100

    Add merge .shp script

---
+++ ArcGIS-ArcPy/README.md
@@ -0,0 +1,8 @@
+# Arcpy
+
+Here are some scripts!
+
+
+
+* mergeShapefiles.py : 
+Simple .shp filse merge script, used to merge Navstreets .shp from multiple subdirectories to a file geodatabase 
---
+++ ArcGIS-ArcPy/mergeShapefiles.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+import os
+import arcpy
+
+
+#Merge a lot of shapefiles with the same name from different subdirectories to a feature class a gdb
+
+#basefolder
+folder = "Z:\\shapeFolder"
+
+#output gdb
+outGdb = "Z:\\outGeodatabase.gdb"
+
+
+# dict with shapefile name as key and array of path as value
+# {'a.shp' : ['c:/dir1/a.shp','c:/dir2/a.shp','c:/dir3/a.shp']}
+shapefileDic = dict()
+
+#loop in all directories and subdirectories
+for path,dirnames,filenames in os.walk(folder):
+        for filename in filenames:
+                if filename.endswith(".shp"):
+                        #get the full path
+                        fullpath =  path+'\\'+filename
+                        #if not already in the dic, add it
+                        if not filename in shapefileDic:
+                            shapefileDic[filename] = [fullpath]
+                        #if it already exist, add the new shapefile path to the array
+                        else:
+                             shapefileDic[filename].append(fullpath)
+
+#we have a dic with all the shapesfiles path
+#print shapefileDic
+
+#merge them
+for filename in shapefileDic:
+        #we strip .shp
+        featurename = filename[:-4]
+        print featurename
+        arcpy.Merge_management(shapefileDic[filename],outGdb+"/"+featurename)
